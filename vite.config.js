5031b3022fc0fcbcaa07c5814841f3d3ae1ce349
Troy Vassalotti <45142012+troyvassalotti@users.noreply.github.com>
2022-12-17 11:25:04 -0500

    update config and package

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,15 @@ export default defineConfig({
   build: {
     lib: {
       entry: "src/index",
-      formats: ["es"],
+      name: "WebComponents",
     },
     rollupOptions: {
       external: ["lit", "d3"],
-      input: {
-        "overlay-caption": "src/components/overlay-caption/index",
-        "light-box": "src/components/light-box/index",
-        "calendar-heatmap": "src/components/calendar-heatmap/index",
-        "cloudy-sky": "src/components/cloudy-sky/index",
-        "lightbox-carousel": "src/components/lightbox-carousel/index",
-        "kb-typewriter": "src/components/kb-typewriter/index",
-      },
       output: {
-        entryFileNames: "[name].js",
-        dir: "dist",
+        globals: {
+          lit: "lit",
+          d3: "d3",
+        },
       },
     },
   },
